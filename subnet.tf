ef7f44f27e006bf7546561a73394abee333d3d77
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-08-14 11:33:16 -0500

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,1 +1,11 @@
+resource "google_compute_subnetwork" "network-test" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc-network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
 
