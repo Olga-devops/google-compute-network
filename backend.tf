d5c80cca5f1980727906ff1d70414dbff86d7895
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-08-13 21:23:27 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,11 @@
-data "google_compute_backend_bucket" "my-backend-bucket" {
-  name = "olga-backend"
-  project = "Olga-test"
+resource "google_compute_backend_bucket" "image_backend" {
+  name        = "backend-bucket"
+  description = "Contains beautiful images"
+  bucket_name = google_storage_bucket.image_bucket.name
+  enable_cdn  = true
+}
+
+resource "google_storage_bucket" "olga_bucket" {
+  name     = "olga-bucket"
+  location = "EU"
 }
