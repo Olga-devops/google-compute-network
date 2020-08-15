b63178d3d436d3b9549098a4090b0264cc0593e2
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-08-15 10:47:47 -0500

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,11 +1,27 @@
-resource "google_compute_subnetwork" "network-test" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
+resource "google_compute_subnetwork" "public_network_1" {
+  name          = "pub-subnetwork-1"
+  ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc-network.id
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
 }
 
+resource "google_compute_subnetwork" "public_network_2" {
+  name          = "pub-subnetwork-2"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc-network.id
+}
+
+resource "google_compute_subnetwork" "private_network_1" {
+  name          = "priv-subnetwork-1"
+  ip_cidr_range = "10.0.101.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc-network.id
+}
+
+resource "google_compute_subnetwork" "private_network_2" {
+  name          = "priv-subnetwork-2"
+  ip_cidr_range = "10.2.102.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc-network.id
+}
